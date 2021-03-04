d49573b13bc26b119ae8c007517f37c456146995
MohKabore <mhkabore@gmail.com>
2021-03-04 23:15:58 +0100

    test_fin-sect3

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+       
+
+        public async Task<Product> getProductByIdAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Product>> getProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        public async Task<IReadOnlyList<ProductType>> getProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+         public async Task<IReadOnlyList<ProductBrand>> getProductBrandsAsync()
+        {
+              return await _context.ProductBrands.ToListAsync();
+        }
+    }
+}
