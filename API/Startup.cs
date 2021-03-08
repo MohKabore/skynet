dc5d11dd72b530e445fd70f017d31c96384e9cd5
MohKabore <mhkabore@gmail.com>
2021-03-08 19:06:50 +0100

    end of section 5 cleaned

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
+            app.UseSwaggerDocumentation();
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
             app.UseRouting();
