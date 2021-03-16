044122c5a4c31dd116fc9116f86b8d251308f213
MohKabore <mhkabore@gmail.com>
2021-03-16 11:40:24 +0100

    end of section 11

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -17,7 +17,10 @@ namespace API.Controllers
         {
             var thing = _context.Products.Find(42);
 
-            if (thing == null) return NotFound(new ApiResponse(404));
+            if (thing == null) 
+            {
+                return NotFound(new ApiResponse(404));
+            }
 
             return Ok();
         }
